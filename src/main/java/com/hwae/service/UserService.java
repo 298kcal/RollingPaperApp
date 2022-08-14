e146bb52403c18ab411d79e0e34ff50c493a10ce
SuHyeon00 <o.ss.u2056@gmail.com>
2022-08-15 01:44:05 +0900

    UserService

--- src/main/java/com/hwae/service/UserService.java
+++ src/main/java/com/hwae/service/UserService.java
@@ -1,7 +1,10 @@
 package com.hwae.service;
 
 import com.hwae.dao.UserRepository;
+import com.hwae.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class UserService {
     private final UserRepository userRepository;
@@ -9,5 +12,28 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(User user) {
+        validateDuplicateUser(user); // 중복 회원 검증
+        userRepository.save(user);
+        return user.getUserId();
+    }
+
+    private void validateDuplicateUser(User user) {
+        userRepository.findUserByNickName(user.getNickName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 닉네임입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<User> findUsers() {
+        return userRepository.findAll();
     }
 }
