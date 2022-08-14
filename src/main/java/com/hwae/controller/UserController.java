7b0d820fecb7ac0323e91b480b8152265588064e
SuHyeon00 <o.ss.u2056@gmail.com>
2022-08-15 01:47:31 +0900

    UserController

---
+++ src/main/java/com/hwae/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.hwae.controller;
+
+import com.hwae.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
